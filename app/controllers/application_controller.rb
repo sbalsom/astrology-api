109410fc4b47702f95d4aa020af5e286c2221fd7
Sadie Balsom <saralogsin@protonmail.com>
2019-10-23 16:56:22 +0200

    undid ssl config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_filter :expire_hsts
+  # before_filter :expire_hsts
 
-  private
-  def expire_hsts
-    response.headers["Strict-Transport-Security"] = 'max-age=0'
-  end
+  # private
+  # def expire_hsts
+  #   response.headers["Strict-Transport-Security"] = 'max-age=0'
+  # end
 
 end
