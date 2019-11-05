b8a653e73eeeeb2cf5864c7b979a3c5151920232
Sadie Balsom <saralogsin@protonmail.com>
2019-11-05 11:24:32 +0100

    adds a logout button

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -20,6 +20,6 @@ end
 
 
 Sidekiq.configure_client do |config|
-  config.redis = { namespace:'Moon-Void', url: 'redis://127.0.0.1:6379/1' }
+  config.redis = { namespace:'Moon-Void', url: 'redis://127.0.0.1:6379/0' }
 end
 
