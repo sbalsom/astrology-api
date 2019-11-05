0b473e891e2e82d0b82c725e403890b6dc872751
Sadie Balsom <saralogsin@protonmail.com>
2019-11-05 12:13:20 +0100

    creates logout button for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,11 @@
 require 'sidekiq'
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
-# require 'sidekiq-scheduler'
-# require 'sidekiq-scheduler/web'
 
 Sidekiq.configure_server do |config|
   config.redis = { namespace:'Astrology-Api', url: (ENV["REDISCLOUD_URL"] ||'redis://127.0.0.1:6379/0') }
 
   config.on(:startup) do
-    # SidekiqScheduler::Scheduler.instance.rufus_scheduler_options = { max_work_threads: 1 }
-    # # Sidekiq.schedule = ConfigParser.parse(File.join(Rails.root, "config/sidekiq_scheduler.yml"), Rails.env)
-    # SidekiqScheduler::Scheduler.instance.reload_schedule!
     schedule_file = 'config/schedule.yml'
     if File.exists?(schedule_file)
       Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
